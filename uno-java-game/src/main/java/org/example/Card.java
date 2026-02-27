ab649f262be9eb048d5a3108980a08993239e244
Miguel Fernández de la Cigoña París <miguel.fdez.paris@gmail.com>
2026-02-27 13:49:17 +0100

    Added some code to the Card class, including a constructor, getters and setters for the color and value of the card, and a toString method for easy representation of the card.

--- uno-java-game/src/main/java/org/example/Card.java
+++ uno-java-game/src/main/java/org/example/Card.java
@@ -2,4 +2,27 @@ package org.example;
 
 public class Card {
     private Color color;
+    private Value value;
+
+    public Card(Value value, Color color) {
+        this.value = value;
+        this.color = color;
+
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
 }
