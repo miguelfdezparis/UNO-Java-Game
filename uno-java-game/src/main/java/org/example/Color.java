1d8494025e705bd56ea9d7b656b2a5bb8f9ef9a5
a25jorgela <jorge.lago.alvarez@gmail.com>
2026-03-05 13:16:31 +0100

    Class color modified

--- uno-java-game/src/main/java/org/example/Color.java
+++ uno-java-game/src/main/java/org/example/Color.java
@@ -1,4 +1,18 @@
-package org.example;
-
 public class Color {
+    public static final String[] COLORES = {"ROJO", "AZUL", "AMARILLO", "VERDE", "NEGRO"};
+
+    private String color;
+
+    public Color(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return color;
+    }
 }
