8aa9715a33f7f0c935c588872796d72854a16719
a25jorgela <jorge.lago.alvarez@gmail.com>
2026-03-05 12:54:02 +0100

    Class Value modified

--- uno-java-game/src/main/java/org/example/Value.java
+++ uno-java-game/src/main/java/org/example/Value.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Value {
+    public static final String[] values;
+
+    private values = new String[]{
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "+2", "+4", "CHANGE_COLOR", "PASS", "TURN_SWAP"
+    };
+
+
 }
