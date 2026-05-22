66cec8012e496c325cf52cfb510a7a45192e4fea
Xurxo <165603353+h0rge@users.noreply.github.com>
2026-05-22 14:34:40 +0200

    Progress on MongoBarajaDAO

--- src/main/java/org/example/db/MongoBarajaDAO.java
+++ src/main/java/org/example/db/MongoBarajaDAO.java
@@ -1,5 +1,30 @@
 package org.example.db;
 
-public class MongoBarajaDAO {
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.example.model.Carta;
 
+import java.util.ArrayList;
+
+public class MongoBarajaDAO implements BarajaDAO {
+
+
+    @Override
+    public void guardarBaraja(ArrayList<Carta> cartas) {
+        try (MongoClient client = DatabaseConnection.crearCliente()) {
+            MongoCollection<Document> coleccionCartas = client.getDatabase("unojavagame").getCollection("Cartas");
+
+            Document coleccionCartas =
+        } catch (Exception e) {
+            System.out.println("Error al conectar: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public ArrayList<Carta> obtenerBaraja() {
+        return null;
+    }
 }
+
